c717006848fb6edbc43924a9270664037a7cdaaa
Vishwas Nahar <vshwsnahar3@gmail.com>
2018-05-10 23:26:46 +0530

    devise changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+    	byebug
+	  user_params.permit({ roles: [] }, :seller, :email, :password, :password_confirmation)
+	end
+  end
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to new_user_session_path, :notice => 'You need to login first'
+    end
+  end
 end
